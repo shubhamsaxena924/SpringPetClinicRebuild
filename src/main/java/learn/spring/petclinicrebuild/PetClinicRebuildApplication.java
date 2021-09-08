b6ca7f40e5ef2ce754d908aa76cb1a117cc78cdf
shubhamsaxena924 <shubham.saxena_cs.ccv19@gla.ac.in>
2021-09-08 17:47:22 +0530

    initial commit

---
+++ src/main/java/learn/spring/petclinicrebuild/PetClinicRebuildApplication.java
@@ -0,0 +1,13 @@
+package learn.spring.petclinicrebuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicRebuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicRebuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/learn/spring/petclinicrebuild/PetClinicRebuildApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.spring.petclinicrebuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicRebuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
